15fd2ccd652964a15fc2b22a7ee26d936e2a8d1b
Massimo Predosin <95051496+Chessinator621@users.noreply.github.com>
2021-12-21 17:53:44 +0000

    server and client both work. They both are able to send and receive. Now we can actually start coding the game.

--- client.py
+++ client.py
@@ -4,6 +4,7 @@
 import socket
 import time
 import threading
+
 #socket for client
 s = socket.socket()
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -16,22 +17,22 @@ running=True
 
 def send(msg):
   #this encodes msg and sets a header which represents msg length, I think
-  msg = msg.encode()
+  msg = f'hello world from client'.encode()
   msg = bytes(f"{len(msg):<{headerSize}}",'utf-8')+msg
   s.send(msg)
-  print("client send")
 
-def receive():
+headerSize=10
+msglen=100 #just to set it to something
+def receive(conn, addr):
+  global headerSize,msglen
   while running:
     msg_buffer = b''
     new_msg = True
     while True:
 
-
-      #so this code basically is the msg buffer. Itruns it. idk i swear i knew what it meant two years ago. for now i guess we can just use it?. I swear i coded this on my own with some help.
-      if (new_msg and len(msg_buffer) < headerSize) \
-        or (not new_msg and len(msg_buffer) < headerSize+msglen):
-        msg = s.recv(1024)
+      #Now i know exactly how this works. I would like to do google meet to explain it.
+      if (len(msg_buffer) < msglen):
+        msg = conn.recv(1024)
         msg_buffer += msg
 
       if new_msg:
@@ -39,18 +40,17 @@ def receive():
         new_msg = False
 
       #this is when it knows it received the whole message, and then decodes it
-      if len(msg_buffer)-headerSize >= msglen: # full message
+      if len(msg_buffer)-headerSize >= msglen:
         decoded_msg = msg_buffer[headerSize:headerSize + msglen].decode()
+        print(headerSize,msglen,"full message: ",decoded_msg)
 
-        if decoded_msg=="hello world from server":
-          print("hello word from server")
+        new_msg = True
+        msg_buffer = msg_buffer[headerSize+msglen:]
 
-        print(decoded_msg)
-      new_msg = True
-      msg_buffer = msg_buffer[headerSize+msglen:]
 
-recv_thread = threading.Thread(target=receive)
+recv_thread = threading.Thread(target=receive, args = (s,server_IP))
 recv_thread.start()
+
 while running:
   send("hello word from client")
   time.sleep(1);
--- server.py
+++ server.py
@@ -16,51 +16,46 @@ print("bound to address ", s.getsockname())
 #socket listens for up to 5 connections
 s.listen(5)
 
-msg=''
-
-connections = []
-
-def send(msg):
-  for i in connections:
-    i.send(msg);
+def send(conn,msg):
+  #this encodes msg and sets a header which represents msg length, I think
+  msg = f'hello world from server'.encode()
+  msg = bytes(f"{len(msg):<{headerSize}}",'utf-8')+msg
+  conn.send(msg)
 
 headerSize=10
-def receive():
-  while running and len(connections)>0:
+msglen=100
+
+def receive(conn, addr):
+  global headerSize,msglen
+  while running:
     msg_buffer = b''
     new_msg = True
     while True:
 
-      #so this code basically is the msg buffer. Itruns it. idk i swear i knew what it meant two years ago. for now i guess we can just use it?. I swear i coded this on my own with some help.
-      if (new_msg and len(msg_buffer) < headerSize) \
-        or (not new_msg and len(msg_buffer) < headerSize+msglen):
+      #Now i know exactly how this works. I would like to do google meet to explain it.
+      if (len(msg_buffer) < msglen):
         msg = conn.recv(1024)
         msg_buffer += msg
+        print(msg_buffer)
 
       if new_msg:
         msglen = int(msg_buffer[:headerSize])
         new_msg = False
-      print(msg_buffer)
 
       #this is when it knows it received the whole message, and then decodes it
-      if len(msg_buffer)-headerSize >= msglen: # full message
+      if len(msg_buffer)-headerSize >= msglen:
         decoded_msg = msg_buffer[headerSize:headerSize + msglen].decode()
+        print(headerSize,msglen,"full message: ",decoded_msg)
 
-        if decoded_msg=="hello world from client":
-          print("hello word from client")
-
-        print(decoded_msg)
-      new_msg = True
-      msg_buffer = msg_buffer[headerSize+msglen:]
-
-recv_thread = threading.Thread(target=receive)
-recv_thread.start()
+        new_msg = True
+        msg_buffer = msg_buffer[headerSize+msglen:]
 
 while running:
   conn, addr = s.accept()
-  connections.append(conn);
-  send("hello world from server")
-  print("new conn")
+  thread = threading.Thread(target = receive, args = (conn, addr))
+  thread.start()
+  send(conn,"hello world from server")
+  print("new conn", threading.activeCount()-1)
 
 
 
